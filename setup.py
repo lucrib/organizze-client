a74814da2af58c15728a1ae054c3a1f807b24019
Lucas Ribeiro <lucasribeiro1990@gmail.com>
2020-01-26 18:47:49 +0000

    Add wheel to setup_requires

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ INSTALL_REQUIRES = [
 
 setup(
     use_scm_version={"write_to": "organizze_client/_version.py"},
-    setup_requires=["setuptools-scm", "setuptools>=40.0"],
+    setup_requires=["setuptools-scm", "wheel", "setuptools>=40.0"],
     # package_dir={"": "organizze_client"},
     install_requires=INSTALL_REQUIRES,
 )
